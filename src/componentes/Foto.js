c2aea3cb5248301ad9bd6c0fdc0a1ba8719821d5
AndersonTaborga <anderson.taborga@gmail.com>
2018-11-15 22:21:36 -0200

    Implementado dinamismo foto perfil, foto principal, nome, curtidas e texto do post.

--- src/componentes/Foto.js
+++ src/componentes/Foto.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-export default class Foto extends Component {
+export default class FotoItem extends Component {
 
     render() {
         return (
             <div className="foto">
-                <FotoHeader/>
-                <img alt="foto" className="foto-src" src="https://instagram.fcgh10-1.fna.fbcdn.net/vp/92e28c7fcce9e8f61d2be7a773b9abed/5C7564A0/t51.2885-15/sh0.08/e35/c0.135.1080.1080/s640x640/43589133_303541320467022_203598300496100535_n.jpg"/>
-                <FotoInfo/>
+                <FotoHeader foto={this.props.foto}/>
+                <img alt="foto" className="foto-src" src={this.props.foto.urlFoto}/>
+                <FotoInfo foto={this.props.foto}/>
                 <FotoAtualizacoes/>
             </div>
         )
@@ -19,14 +19,14 @@ class FotoHeader extends  Component {
         return(
             <header className="foto-header">
                 <figure className="foto-usuario">
-                    <img src="https://instagram.fcgh10-1.fna.fbcdn.net/vp/029341c53dbd9b189bbc25081ad296a5/5C63DC00/t51.2885-19/s150x150/40409347_280801122534595_9138464368818651136_n.jpg" alt="foto do usuario"/>
+                    <img src={this.props.foto.urlPerfil} alt="foto do usuario"/>
                     <figcaption className="foto-usuario">
                         <a href="#">
-                            alots
+                            {this.props.foto.loginUsuario}
                         </a>
                     </figcaption>
                 </figure>
-                <time className="foto-data">03/10/2016 20:13</time>
+                <time className="foto-data">{this.props.foto.horario}</time>
             </header>
         )
     }
@@ -38,38 +38,30 @@ class FotoInfo extends Component {
             <div className="foto-in fo">
                 <div className="foto-info-likes">
 
-                    <a href="#">
-                        alots_ssa
-                    </a>
-
-                    ,
-
-                    <a href="#">
-                        rafael_rollo
-                    </a>
-
+                    {
+                        this.props.foto.likers.map(liker => {
+                            return  <a href="#">{liker.login},</a>
+                        })
+                    }
                     curtiram
 
                 </div>
 
                 <p className="foto-info-legenda">
                     <a className="foto-info-autor">autor </a>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, illo?
+                    {this.props.foto.comentario}
                 </p>
 
                 <ul className="foto-info-comentarios">
-                    <li className="comentario">
-                        <a className="foto-info-autor">seguidor </a>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ad, molestiae.
-                    </li>
-                    <li className="comentario">
-                        <a className="foto-info-autor">seguidor </a>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt cumque earum molestias voluptatem modi nihil sit magnam ratione eveniet distinctio magni error asperiores dignissimos tempora expedita, laborum ex soluta hic maiores veritatis deserunt.
-                    </li>
-                    <li className="comentario">
-                        <a className="foto-info-autor">seguidor </a>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum laudantium quae ab fuga odio delectus maiores voluptatibus sit commodi quidem.
-                    </li>
+                    {this.props.foto.comentarios.map(comentario => {
+                        return (
+                            <li className="comentario">
+                                <a className="foto-info-autor">{comentario.login} </a>
+                                {comentario.texto}
+                            </li>
+                        )
+                    })}
+
                 </ul>
             </div>
         );
