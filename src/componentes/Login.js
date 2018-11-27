cb4da5bf7b82f1e806628ce4bf5c56e30756362c
AndersonTaborga <anderson.taborga@gmail.com>
2018-11-26 22:47:12 -0200

    Implementação do like, comentarios, pesquisa, transition e presetatinal

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,56 +1,52 @@
 import React, { Component } from 'react';
-import {browserHistory} from 'react-router';
+import {browserHistory} from  'react-router';
 
 export default class Login extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {msg: this.props.location.query.msg};
+    constructor(props){
+        super(props);        
+        this.state = {msg:this.props.location.query.msg};
     }
 
-    /* Sempre que fizermos um Ajax, precisamos interromper o fluxo natural do carregamento da DOM */
-    envia(event) {
+    envia(event){
         event.preventDefault();
 
         const requestInfo = {
-            method: 'POST',
-            body: JSON.stringify({login: this.login.value,
-                senha: this.senha.value}),
-            headers: new Headers({
-                'Content-type':'application/json'
+            method:'POST',
+            body:JSON.stringify({login:this.login.value,senha:this.senha.value}),
+            headers:new Headers({
+                'Content-type' : 'application/json' 
             })
-        }
+        };
 
-        fetch('https://instalura-api.herokuapp.com/api/public/login', requestInfo)
+        fetch('https://instalura-api.herokuapp.com/api/public/login',requestInfo)
             .then(response => {
-                if (response.ok){
+                if(response.ok) {
                     return response.text();
-                }else {
-                    // this.setState({msg: 'Não foi possível fazer login.'})
-                    throw new Error('Não foi possivel fazer o login.');
+                } else {
+                    throw new Error('não foi possível fazer o login');
                 }
             })
             .then(token => {
-                localStorage.setItem('auth-token', token);
+                localStorage.setItem('auth-token',token);
                 browserHistory.push('/timeline');
             })
             .catch(error => {
-                this.setState({msg: error.message});
-            })
+                this.setState({msg:error.message});
+            });
     }
 
-    render() {
+    render(){
         return (
-          <div className='login-box'>
-              <h1 className='header-logo'>Instalura</h1>
-              <span>{this.state.msg}</span>
-              <form onSubmit={this.envia.bind(this)}>
-                  <input type="text" ref={(input) => this.login = input}/>
-                  <input type="password" ref={(input) => this.senha = input}/>
-                  <input type="submit" value='login'/>
-              </form>
-          </div>
+            <div className="login-box">
+                <h1 className="header-logo">Instalura</h1>
+                <span>{this.state.msg}</span>
+                <form onSubmit={this.envia.bind(this)}>
+                    <input type="text" ref={(input) => this.login = input}/>
+                    <input type="password" ref={(input) => this.senha = input}/>
+                    <input type="submit" value="login"/>
+                </form>
+            </div>
         );
     }
-
 }
