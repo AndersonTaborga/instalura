1ffc910ce506f8c0b3ee4e659ed59c927298cfea
Anderson Taborga <anderson.taborga@gmail.com>
2018-12-01 21:31:14 -0200

    Código refatorado para seguir o padrão Flux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import Header from './componentes/Header.js'
-import Timeline from './componentes/Timeline.js'
+import React, { Component } from "react";
+import Header from "./componentes/Header.js";
+import Timeline from "./componentes/Timeline.js";
+import TimelineStore from "./logicas/TimelineStore";
+
+const timelineStore = new TimelineStore([]);
 
 class App extends Component {
-    render() {
-        return (
-            <div id="root">
-                <div className="main">
-                    <Header />
-                    <Timeline login={this.props.params.login} />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div id="root">
+        <div className="main">
+          <Header />
+          <Timeline login={this.props.params.login} store={timelineStore} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -1,28 +1,17 @@
 import React, { Component } from "react";
 import FotoItem from "./Foto";
-import Pubsub from "pubsub-js";
 import ReactCSSTransitionGroup from "react/lib/ReactCSSTransitionGroup";
-import LogicaTimeline from "../logicas/logicaTimeline";
 
 export default class Timeline extends Component {
   constructor(props) {
     super(props);
     this.state = { fotos: [] };
     this.login = this.props.login;
-    this.logicaTimeline = new LogicaTimeline();
   }
 
   componentWillMount() {
-    Pubsub.subscribe("timeline", (topico, fotos) => {
+    this.props.store.subscribe(fotos => {
       this.setState({ fotos });
-    });
-
-    Pubsub.subscribe("novos-comentarios", (topico, infoComentario) => {
-      const fotoAchada = this.state.fotos.find(
-        foto => foto.id === infoComentario.fotoId
-      );
-      fotoAchada.comentarios.push(infoComentario.novoComentario);
-      this.setState({ fotos: this.state.fotos });
     });
   }
 
@@ -39,12 +28,7 @@ export default class Timeline extends Component {
       }`;
     }
 
-    fetch(urlPerfil)
-      .then(response => response.json())
-      .then(fotos => {
-        this.setState({ fotos: fotos });
-        this.logicaTimeline = new LogicaTimeline(fotos);
-      });
+    this.props.store.lista(urlPerfil);
   }
 
   componentDidMount() {
@@ -59,34 +43,10 @@ export default class Timeline extends Component {
   }
 
   like(fotoId) {
-    this.logicaTimeline.like(fotoId);
+    this.props.store.like(fotoId);
   }
-
   comenta(fotoId, textoComentario) {
-    const requestInfo = {
-      method: "POST",
-      body: JSON.stringify({ texto: textoComentario }),
-      headers: new Headers({
-        "Content-type": "application/json"
-      })
-    };
-
-    fetch(
-      `https://instalura-api.herokuapp.com/api/fotos/${fotoId}/comment?X-AUTH-TOKEN=${localStorage.getItem(
-        "auth-token"
-      )}`,
-      requestInfo
-    )
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("não foi possível comentar");
-        }
-      })
-      .then(novoComentario => {
-        Pubsub.publish("novos-comentarios", { fotoId, novoComentario });
-      });
+    this.props.store.comenta(fotoId, textoComentario);
   }
 
   render() {
@@ -102,7 +62,7 @@ export default class Timeline extends Component {
               key={foto.id}
               foto={foto}
               like={this.like.bind(this)}
-              comenta={this.comenta}
+              comenta={this.comenta.bind(this)}
             />
           ))}
         </ReactCSSTransitionGroup>
